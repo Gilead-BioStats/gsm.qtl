2864100abf8187550ca305d63303d4e79fe2b3a3
zdz2101 <zelos.zhu@atorusresearch.com>
2025-04-22 12:51:09 -0700

    add basic mapping example

---
+++ inst/examples/mapping_example.R
@@ -0,0 +1,23 @@
+library(gsm.datasim) # requires >= fix-50
+library(gsm.mapping) # requires >= fix-58
+library(gsm.core)
+
+# generate gilda data for monitoring visit app:
+ie_data <- generate_rawdata_for_single_study(
+  SnapshotCount = 1,
+  SnapshotWidth = "months",
+  ParticipantCount = 1000,
+  SiteCount = 50,
+  StudyID = "ABC",
+  workflow_path = "workflow",
+  mappings = "IE",
+  package = "gsm.mapping",
+  desired_specs = NULL
+)
+
+lSource <- ie_data[[1]]
+# Step 1 - Create Mapped Data Layer - filter, aggregate and join raw data to create mapped data layer
+mappings_wf <- gsm.core::MakeWorkflowList(strNames = "IE", strPath = "workflow/1_mappings", strPackage = "gsm.mapping")
+mappings_spec <- gsm.mapping::CombineSpecs(mappings_wf)
+lRaw <- gsm.mapping::Ingest(lSource, mappings_spec)
+mapped <- gsm.core::RunWorkflows(mappings_wf, lRaw)
