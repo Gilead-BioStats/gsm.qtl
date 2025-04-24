e4c197a46e97c91a0eec9888774de4b27f0721ef
zdz2101 <zelos.zhu@atorusresearch.com>
2025-04-24 12:55:14 -0700

    timeSeries just needs different grouping ability, before clicking?

--- inst/examples/mapping_example.R
+++ inst/examples/mapping_example.R
@@ -1,10 +1,12 @@
 library(gsm.datasim) # requires >= fix-50
 library(gsm.mapping) # requires >= fix-58
+library(gsm.reporting)
 library(gsm.core)
-
+devtools::load_all()
+set.seed(1234)
 # generate gilda data for monitoring visit app:
 ie_data <- generate_rawdata_for_single_study(
-  SnapshotCount = 1,
+  SnapshotCount = 6,
   SnapshotWidth = "months",
   ParticipantCount = 1000,
   SiteCount = 50,
@@ -15,13 +17,39 @@ ie_data <- generate_rawdata_for_single_study(
   desired_specs = NULL
 )
 
-lSource <- ie_data[[1]]
-# Step 1 - Create Mapped Data Layer - filter, aggregate and join raw data to create mapped data layer
-mappings_wf <- gsm.core::MakeWorkflowList(strNames = c("IE", "ENROLL"), strPath = "workflow/1_mappings", strPackage = "gsm.mapping")
-mappings_spec <- gsm.mapping::CombineSpecs(mappings_wf)
-lRaw <- gsm.mapping::Ingest(lSource, mappings_spec)
-mapped <- gsm.core::RunWorkflows(mappings_wf, lRaw)
+analyzed <- list()
+reporting <- list()
+dates <- names(ie_data) %>% as.Date
+for(snap in seq_along(ie_data)){
+  lSource <- ie_data[[snap]]
 
-# Step 2 - Create Metrics - calculate metrics using mapped data
-metrics_wf <- gsm.core::MakeWorkflowList(strPath = "inst/workflow/2_metrics", strPackage = "gsm.qtl")
-analyzed <- gsm.core::RunWorkflows(metrics_wf, mapped)
+  mappings_wf <- gsm.core::MakeWorkflowList(strNames =c("SUBJ", "ENROLL", "IE", "STUDY", "SITE", "COUNTRY"), strPath = "workflow/1_mappings", strPackage = "gsm.mapping")
+  mappings_spec <- gsm.mapping::CombineSpecs(mappings_wf)
+  lRaw <- gsm.mapping::Ingest(lSource, mappings_spec)
+
+  # Step 1 - Create Mapped Data Layer - filter, aggregate and join raw data to create mapped data layer
+  mapped <- gsm.core::RunWorkflows(mappings_wf, lRaw)
+
+  # Step 2 - Create Metrics - calculate metrics using mapped data
+  metrics_wf <- gsm.core::MakeWorkflowList(strPath = "inst/workflow/2_metrics", strPackage = "gsm.qtl")
+  analyzed[[snap]] <- gsm.core::RunWorkflows(metrics_wf, mapped)
+
+  # # Step 3 - Create Reporting Layer - create reports using metrics data
+  reporting_wf <- gsm.core::MakeWorkflowList(strPath = "workflow/3_reporting", strPackage = "gsm.reporting")
+  reporting[[snap]] <- gsm.core::RunWorkflows(reporting_wf, c(mapped, list(lAnalyzed = analyzed[[snap]],
+                                                                           lWorkflows = metrics_wf)))
+  reporting[[snap]]$Reporting_Results$SnapshotDate = dates[snap]
+  reporting[[snap]]$Reporting_Bounds$SnapshotDate = dates[snap]
+}
+
+all_reportingResults <- do.call(dplyr::bind_rows, lapply(reporting, function(x) x$Reporting_Results))
+all_reportingGroups <- reporting[[snap]]$Reporting_Groups
+all_reportingBounds <- do.call(dplyr::bind_rows, lapply(reporting, function(x) x$Reporting_Bounds))
+all_reportingMetrics <- reporting[[snap]]$Reporting_Metrics
+
+lCharts <- gsm.kri::MakeCharts(
+  dfResults = all_reportingResults,
+  dfGroups = all_reportingGroups,
+  dfMetrics = all_reportingMetrics,
+  dfBounds =all_reportingBounds
+)
