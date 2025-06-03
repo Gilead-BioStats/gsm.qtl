9b60b3c6002a42bbae4f1e7baedbe216e48742bd
zdz2101 <zelos.zhu@atorusresearch.com>
2025-06-03 10:36:43 -0700

    add some tweaks to get package docs going

---
+++ R/gsm.qtl-package.R
@@ -0,0 +1,13 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @import dplyr
+#' @import ggplot2
+#' @import gsm.core
+#' @import gt
+#' @import purrr
+#' @import rlang
+#' @import tidyr
+## usethis namespace: end
+NULL
