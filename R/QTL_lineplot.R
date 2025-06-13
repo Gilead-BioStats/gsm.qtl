6f6999d7043fffa1b44b538f8c3863ec3a734e2f
zdz2101 <zelos.zhu@atorusresearch.com>
2025-06-13 10:50:37 -0700

    undo it

--- R/QTL_lineplot.R
+++ R/QTL_lineplot.R
@@ -20,8 +20,6 @@ QTL_lineplot <- function(dfResults,
       TRUE ~ strQTL
     ))
 
-
-
   # Pivot thresholds to wide format
   thresholds <- df_plot %>%
     filter(GroupID %in% c("Upper_funnel", "Flatline")) %>%
@@ -39,15 +37,12 @@ QTL_lineplot <- function(dfResults,
         group_type != strQTL ~ NA_character_,
         Metric > `Upper_funnel` ~ "red",
         TRUE ~ "green"
+      ),
+      tooltip_text = case_when(
+        group_type != strQTL ~ paste0(group_type, "\nDate: ", SnapshotDate, "\nMetric: ", Metric),
+        TRUE ~ paste0("Study: ", GroupID, "\nDate: ", SnapshotDate, "\nMetric: ", Metric, "\nNumerator: ", Numerator, "\nDenominator: ", Denominator)
       )
-      # ,
-      # tooltip_text = case_when(
-      #   group_type != strQTL ~ paste0(group_type, "\nDate: ", SnapshotDate, "\nMetric: ", Metric),
-      #   TRUE ~ paste0("Study: ", GroupID, "\nDate: ", SnapshotDate, "\nMetric: ", Metric, "\nNumerator: ", Numerator, "\nDenominator: ", Denominator)
-      # )
     )
-
-
 
   # Build ggplot
   p <- ggplot(
@@ -78,33 +73,32 @@ QTL_lineplot <- function(dfResults,
       size = 1,
       show.legend = FALSE
     ) +
-    # # Combine all colors in scale but only show legend for line groups
-    # scale_color_manual(
-    #   values = c(
-    #     "QTL Threshold" = "#FF5859",
-    #     "Nominal Threshold" = "grey80",
-    #     strQTL = "grey50",
-    #     "red" = "#FF5859",
-    #     "green" = "#3DAF06"
-    #   ),
-    #   breaks = c(strQTL, "Nominal Threshold", "QTL Threshold"),
-    #   name = NULL
-    # ) +
-    # scale_linetype_manual(
-    #   values = c(
-    #     "QTL Threshold" = "dashed",
-    #     "Nominal Threshold" = "dotted",
-    #     strQTL = "solid"
-    #   )
-    # ) +
-    # scale_linewidth_manual(
-    #   values = c(
-    #     "QTL Threshold" = 0.75,
-    #     "Nominal Threshold" = 0.5,
-    #     strQTL = 0.75
-    #   )
-    # ) +
-
+    # Combine all colors in scale but only show legend for line groups
+    scale_color_manual(
+      values = c(
+        "QTL Threshold" = "#FF5859",
+        "Nominal Threshold" = "grey80",
+        strQTL = "grey50",
+        "red" = "#FF5859",
+        "green" = "#3DAF06"
+      ),
+      breaks = c(strQTL, "Nominal Threshold", "QTL Threshold"),
+      name = NULL
+    ) +
+    scale_linetype_manual(
+      values = c(
+        "QTL Threshold" = "dashed",
+        "Nominal Threshold" = "dotted",
+        strQTL = "solid"
+      )
+    ) +
+    scale_linewidth_manual(
+      values = c(
+        "QTL Threshold" = 0.75,
+        "Nominal Threshold" = 0.5,
+        strQTL = 0.75
+      )
+    ) +
     # scale_x_date(date_labels = "%Y-%m-%d", date_breaks = "1 month") +
     theme_minimal() +
     theme(
@@ -115,11 +109,5 @@ QTL_lineplot <- function(dfResults,
       y = strQTL,
       x = "Snapshot Date"
     )
-  return(p)
-  #
-  #
-  #
-  #
-  #
-  # ggplotly(p, tooltip = "text")
+  ggplotly(p, tooltip = "text")
 }
