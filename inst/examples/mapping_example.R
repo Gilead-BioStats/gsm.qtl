d9d8a8853241e45d6fc3c13602e98d8f053e90ac
zdz2101 <zelos.zhu@atorusresearch.com>
2025-07-10 13:05:34 -0700

    modify thresholds

--- inst/examples/mapping_example.R
+++ inst/examples/mapping_example.R
@@ -27,7 +27,7 @@ mappings_wf <- gsm.core::MakeWorkflowList(
   strPackage = "gsm.mapping"
 )
 mappings_spec <- gsm.mapping::CombineSpecs(mappings_wf)
-metrics_wf <- gsm.core::MakeWorkflowList(strNames = c("qtl0001", "qtl0002"), strPath = "workflow/2_metrics", "gsm.qtl")
+metrics_wf <- gsm.core::MakeWorkflowList(strNames = c("qtl0001", "qtl0002"), strPath = "inst/workflow/2_metrics")
 reporting_wf <- gsm.core::MakeWorkflowList(strNames = c("Results", "Groups"), strPath = "workflow/3_reporting", strPackage = "gsm.reporting")
 
 lRaw <- map_depth(ie_data, 1, gsm.mapping::Ingest, mappings_spec)
@@ -66,9 +66,10 @@ gsm.kri::RenderRmd(
   strOutputFile = "test.html",
   strInputPath = system.file("report/Report_QTL.Rmd", package = "gsm.qtl")
 )
-# example_lparams <- list(
-# dfResults = all_reportingResults,
-# dfGroups = all_reportingGroups,
-# lListings = report_listings
-# )
-# usethis::use_data(example_lparams, internal = TRUE, overwrite = TRUE)
+
+example_lparams <- list(
+  dfResults = all_reportingResults,
+  dfGroups = all_reportingGroups,
+  lListings = report_listings
+)
+usethis::use_data(example_lparams, internal = TRUE, overwrite = TRUE)
