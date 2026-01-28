2e41942bd78bdccb74a8edba47805e97d8fb393b
zdz2101 <zelos.zhu@atorusresearch.com>
2026-01-28 12:41:08 -0800

    rephrase the PD without EDC I/E

--- R/criteria_groupBar.R
+++ R/criteria_groupBar.R
@@ -15,7 +15,7 @@ criteria_groupBar <- function(df, varGroupID, strGroupLabel) {
   df_counts <- df %>%
     filter(!is.na(ietestcd_concat) | Source == "Eligibility IPD") %>%
     tidyr::separate_longer_delim(ietestcd_concat, ";;;") %>%
-    mutate(ietestcd_concat = ifelse(Source == "Eligibility IPD", "No EDC I/E available", ietestcd_concat))%>%
+    mutate(ietestcd_concat = ifelse(Source == "Eligibility IPD", "PD without EDC I/E", ietestcd_concat))%>%
     dplyr::count(ietestcd_concat, !!var_sym, name = "n")
 
   distinct_n_ie <- df_counts %>% dplyr::distinct(ietestcd_concat) %>% nrow()
