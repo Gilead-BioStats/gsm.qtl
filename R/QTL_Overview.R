9b15faf183b8ff74f644f4490ad480dff42a3c42
zdz2101 <zelos.zhu@atorusresearch.com>
2026-02-26 12:35:23 -0800

    missed one of the roundings

--- R/QTL_Overview.R
+++ R/QTL_Overview.R
@@ -23,7 +23,7 @@ QTL_Overview <- function(dfResults,
     ) %>%
     mutate_at(c("Metric", "Upper_funnel", "Numerator", "Denominator", "Flag"), as.numeric) %>%
     mutate(
-      qtlrate = paste0(as.character(round(Metric * 100, 3)), "%"),
+      qtlrate = paste0(as.character(round(Metric * 100, 1)), "%"),
       Deviation = ifelse(Metric > Upper_funnel, "Yes", "No"),
       Upper_funnel = paste0(as.character(round(Upper_funnel * 100, 1)), "%")
     ) %>%
