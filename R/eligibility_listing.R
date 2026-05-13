922032fa17b3990aaea4e3f5833bfac326a006ff
Jon Harmon <jon.harmon@atorusresearch.com>
2026-05-13 08:53:43 -0500

    Update workflows (#106)
    * Update workflows
    
    Closes #105
    
    * Use default token in gh so it (hopefully) builds properly
    
    * Make R CMD check happy about global variables
    
    * Secrets & speed
    
    * Delete deprecated requirement issue template
    
    ---------
    
    Co-authored-by: Jon Harmon <jon.harmon1@gilead.com>
    Co-authored-by: Laura Maxwell <laura.maxwell@atorusresearch.com>

--- R/eligibility_listing.R
+++ R/eligibility_listing.R
@@ -17,63 +17,77 @@ eligibility_listing <- function(df, download = FALSE) {
   n_elig <- if (length(elig_splits) == 0) 1L else max(lengths(elig_splits))
 
   listing <- df %>%
-    select(country, invid, subjid, Source, ietestcd_concat, dvdtm, eligibility_criteria) %>%
+    select(
+      "country",
+      "invid",
+      "subjid",
+      "Source",
+      "ietestcd_concat",
+      "dvdtm",
+      "eligibility_criteria"
+    ) %>%
     tidyr::separate(
-      dvdtm,
+      "dvdtm",
       into = paste0("PD Date", seq_len(n_dvdtm)),
       sep = ";;;",
       fill = "right"
     ) %>%
     mutate_at(vars(starts_with("PD Date")), ~ substr(.x, 1, 10)) %>%
     tidyr::separate(
-      eligibility_criteria,
+      "eligibility_criteria",
       into = paste0("PD Term", seq_len(n_elig)),
       sep = ";;;",
       fill = "right"
     ) %>%
     rename(
-      Country = country,
-      Site = invid,
-      `Participant ID` = subjid,
-      `Which I/E` = ietestcd_concat
+      Country = "country",
+      Site = "invid",
+      `Participant ID` = "subjid",
+      `Which I/E` = "ietestcd_concat"
     ) %>%
-    arrange(Site, `Participant ID`)
+    arrange(.data$Site, .data$`Participant ID`)
 
-  if(download == TRUE) {
+  if (download == TRUE) {
     return(listing)
-  }
-  else {
+  } else {
     listing %>%
       gt::gt() %>%
       text_transform(
-        locations = cells_body(columns = c(`Which I/E`, starts_with("PD Term"))),
+        locations = cells_body(
+          columns = c(`Which I/E`, starts_with("PD Term"))
+        ),
         fn = function(x) {
-          vapply(x, function(txt) {
-            if (is.na(txt) || !nzchar(txt)) {
-              return("")
-            }
+          vapply(
+            x,
+            function(txt) {
+              if (is.na(txt) || !nzchar(txt)) {
+                return("")
+              }
 
-            # Tooltip: escape to be safe
-            tooltip <- htmlEscape(txt)
+              # Tooltip: escape to be safe
+              tooltip <- htmlEscape(txt)
 
-            # Visual truncation logic based on plain text
-            plain <- gsub("<[^>]+>", "", txt)
-            should_truncate <- nchar(plain) > 10
+              # Visual truncation logic based on plain text
+              plain <- gsub("<[^>]+>", "", txt)
+              should_truncate <- nchar(plain) > 10
 
-            display_html <- if (should_truncate) {
-              # You could optionally also truncate the HTML, but that's tricky.
-              txt # just use full HTML; visual cutoff will be via CSS
-            } else {
-              txt
-            }
+              display_html <- if (should_truncate) {
+                # You could optionally also truncate the HTML, but that's tricky.
+                txt # just use full HTML; visual cutoff will be via CSS
+              } else {
+                txt
+              }
 
-            html(
-              sprintf(
-                '<div title="%s" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis; max-width: 100px;">%s</div>',
-                tooltip, display_html
+              html(
+                sprintf(
+                  '<div title="%s" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis; max-width: 100px;">%s</div>',
+                  tooltip,
+                  display_html
+                )
               )
-            )
-          }, FUN.VALUE = html(""))
+            },
+            FUN.VALUE = html("")
+          )
         }
       ) %>%
       scrollable_gt(., height = "500px")
@@ -87,14 +101,21 @@ eligibility_listing <- function(df, download = FALSE) {
 #'
 #' @returns A `gt` object
 #' @export
-scrollable_gt <- function(gt_tbl, height = "300px", min_table_width = "1200px") {
+scrollable_gt <- function(
+  gt_tbl,
+  height = "300px",
+  min_table_width = "1200px"
+) {
   div(
-    style = sprintf("
+    style = sprintf(
+      "
       max-height: %s;
       overflow-y: auto;
       overflow-x: auto;
       border: 1px solid #ccc;
-    ", height),
+    ",
+      height
+    ),
     div(
       style = sprintf("min-width: %s;", min_table_width),
       gt_tbl
